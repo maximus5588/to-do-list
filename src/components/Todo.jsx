89ba2b16070c86452999b02f7b5341ab3a946b63
maximus5588 <adarshks20002@gmail.com>
2025-09-02 14:48:00 +0530

    Add Todo component with correct casing

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const todo = () => {
+export const Todo = ({task}) => {
   return (
-    <div>todo</div>
+    <div className='todo'>
+      <p>{task.task}</p>
+      <div>
+      <FontAwesomeIcon icon={faPenToSquare}/>
+  
+          <FontAwesomeIcon icon={faTrash}/>
+      </div>
+    </div>
   )
 }
+
+export default Todo;
